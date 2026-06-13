0d3e280a472c16acc2735a4ee4bde1072e9985aa
piprock <yura.losov@gmail.com>
2026-06-13 11:38:11 +0300

    First migrations

--- tests_app/views.py
+++ tests_app/views.py
@@ -1,43 +1,14 @@
 from django.shortcuts import render
-
-tests = [
-    {
-        'id': 1,
-        'name': 'Додавання і віднімання раціональних чисел ',
-        'description': 'Математика 6 клас',
-        'author' : 'Яремків О. В.',
-        'image': 'https://www.englishdom.com/dynamicus/blog-post/000/002/256/1621948173_content_700x455.jpg',
-    },
-    {
-        'id': 2,
-        'name': 'Дієслово ',
-        'description': 'Українська мова 4 клас',
-        'author' : 'Некрашевич С. А.',
-        'image': 'https://www.bsmu.edu.ua/wp-content/uploads/2023/10/003axj-dbc4.jpg',
-    },
-    {
-        'id': 3,
-        'name': '"Різдвяна пісня в прозі" ',
-        'description': 'Зарубіжна література 6 клас',
-        'author' : 'Бовда О. ',
-        'image': 'https://naurok-test.nyc3.cdn.digitaloceanspaces.com/145717/images/502457_1557645597.jpg',
-    },
-]
-
-
+from tests_app.models import Test
 
 # Create your views here.
 def tests_list(request):
+    tests = Test.objects.all()
     return render(request, 'tests/tests_list.html', {'tests' : tests})
 
 def about(request):
     return render(request, 'tests/about.html')
 
 def test_detail(request, test_id):
-    test = None
-    for t in tests:
-        if t['id'] == test_id:
-            test = t
-            break
-        
+    test = Test.objects.get(id=test_id)
     return render(request, 'tests/test_detail.html', {'test' : test})
